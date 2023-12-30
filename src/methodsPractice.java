064e6fb03fed64d488b6c5495abba4c8d3429e96
Sony <butreddymeghana@gmail.com>
2023-12-30 10:28:10 +0530

    1stday

---
+++ src/methodsPractice.java
@@ -0,0 +1,23 @@
+public class methodsPractice {
+    public static void main(String[] args) {
+    sony();
+double c = add( 5,6);
+        System.out.println(c);
+ c= add(7,0);
+        System.out.println(c);
+    }
+
+    public static double add(double a , int b ){
+
+        return (a+b);
+    }
+    public static int subtract(int a, int b) {
+        int c = (a - b);
+        return c;
+    }
+        public static String sony(){
+            return "cooking";
+
+
+    }
+}
