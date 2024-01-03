c152fda908867ee4c435cc83aa3ef307d32a5327
manojleolappy <manojr1r@gmail.com>
2024-01-03 11:24:32 +0530

    newYear

---
+++ src/SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+    public static void main(String[] args) {
+        double milesPerHour = 1.609;
+    }
+long conversion = toMilesPerHour(1.5);
+
+    }
+
+        public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            return -1;
+        }
+        else{
+            System.out.println(kilometersPerHour * toMilesPerHour);
+        }
+
+        }
+public static void printConversion(double kilometersPerHour) {
+
+}
