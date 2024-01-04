52d72106a9aac5aced455c5db1db2c96a3cf78b4
manojleolappy <manojr1r@gmail.com>
2024-01-04 12:29:02 +0530

    MergeConflicts

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -15,9 +15,7 @@ public class SpeedConverter {
     public static void printConversion(double kiloMetersPerHour){
         if(kiloMetersPerHour<0){
             System.out.print("Invalid value");
-
             return;
-
         }
 long milesPerHour = toMilesPerHour(kiloMetersPerHour);
         System.out.println( kiloMetersPerHour + " km/h = " + milesPerHour + " mi/h");
